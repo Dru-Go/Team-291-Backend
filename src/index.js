b94f5d9af5a2d7a76cc4b6862eeead6a85f14bf9
Dru-Go <drumac2@gmail.com>
2020-08-26 20:11:47 +0300

    - Added an ESlint tool to notify me of errors so i could reduce the time debuging wih codacy since codcy uses the same linter - Modified some linting rule to make it work with my spacing choice - Updated the readme to display the results of Codacy
    Just deployed to heroku
    - required a lot of commits as i was having issues
    - Updated my readme to reflect the changes and show my latest progress
    - FIxed #5

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// ANCHOR initialize the server and graphql, set routes and port for the server
+import express from 'express';
+import expressGraphQl from 'express-graphql';
+import Schema from './schema';
+import root from './resolver';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+// Initialize Express server
+const app = express();
+
+// Set Routes
+app.get('/', (req, res) => {
+	return res.json({
+		msg: 'Welcome to my GQL World'
+	});
+});
+
+// Use Cross origin resourse sharing to manage the security of the server
+app.use(cors());
+
+// Use express to initalize graphqql
+app.use(
+	'/graphql',
+	expressGraphQl({
+		schema: Schema,
+		rootValue: root, // Note root is where the resolvers are stored
+		graphiql: true
+	})
+);
+
+// Listen for the port
+dotenv.config();
+app.listen(process.env.PORT);
--- src/schema.js
+++ src/schema.js
@@ -1,7 +1,7 @@
 // ANCHOR we define the schema which are used to structure the data flow b/n routes
-import { buildSchema } from "graphql";
+import { buildSchema } from 'graphql';
 
-const schema = buildSchema(`
+const schema = buildSchema(` 
 type Query {
 	hello: String
 }
