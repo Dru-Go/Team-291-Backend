f8d3287a4bef589232cd2bde65728b101a6badf1
Dru-Go <drumac2@gmail.com>
2020-11-04 05:27:53 +0300

    Hotfix: Unnecessary block in codacy

--- src/resolvers/queries/accounts.js
+++ src/resolvers/queries/accounts.js
@@ -5,27 +5,27 @@ import bcrypt from 'bcrypt';
 
 // NOTE Get all accounts stored in the database
 export const accounts = async () => {
-	return await User.find().catch((error) => new Error(error));
+  return await User.find().catch((error) => new Error(error));
 };
 
 // NOTE login to the system using email and a password
 export const login = async ({ input }) => {
-	const account = await User.findOne({ phone: input.phone });
-	if (!account) {
-		throw new Error('Account does not exist');
-	}
-	console.log(input.password === account.password);
-	const isEqual = bcrypt.compare(input.password, account.password);
+  const account = await User.findOne({ phone: input.phone });
+  if (!account) {
+    throw new Error('Account does not exist');
+  }
+  console.log(input.password === account.password);
+  const isEqual = bcrypt.compare(input.password, account.password);
 
-	if (!isEqual) {
-		throw new Error('Password is incorrect');
-	}
-	const token = JWT.sign(
-		{ driverId: account.id, phone: account.phone },
-		'teamtwonineonegroupasupersecretkey',
-		{
-			expiresIn: '1h'
-		}
-	);
-	return { driverId: account.id, token: token, tokenExpiration: 1 };
+  if (!isEqual) {
+    throw new Error('Password is incorrect');
+  }
+  const token = JWT.sign(
+    { driverId: account.id, phone: account.phone },
+    'teamtwonineonegroupasupersecretkey',
+    {
+      expiresIn: '1h'
+    }
+  );
+  return { driverId: account.id, token, tokenExpiration: 1 };
 };
